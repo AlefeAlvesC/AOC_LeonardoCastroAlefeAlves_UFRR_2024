f293da1b9927718ec62a60f7b1816e724a847680
Alefe <alefealvesdacosta@gmail.com>
2025-03-17 13:53:08 -0400

    Generalizando arquivo do banco de registradores

--- 8-bit-cpu/banco_de_registradores.vhd
+++ 8-bit-cpu/banco_de_registradores.vhd
@@ -5,16 +5,22 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY banco_de_registradores IS
+    
+    generic( 
+        N	:	integer	:= 8;--Tamanho dos dados no banco de registradores
+        E	:	integer	:= 2 --Qtde de palavra no banco de registradores
+    );
+
     PORT(
         CLOCK,REG_WRITE   : IN STD_LOGIC;
-        REG1_IN,REG2_IN   : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        WRITE_DATA        : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-        REG1_OUT,REG2_OUT : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+        REG1_IN,REG2_IN   : IN STD_LOGIC_VECTOR(E-1 DOWNTO 0);
+        WRITE_DATA        : IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+        REG1_OUT,REG2_OUT : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0)
     );
 END banco_de_registradores;
 
 ARCHITECTURE BEHAVIOR OF banco_de_registradores IS
-TYPE banco_de_registradores IS ARRAY(0 TO 3) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
+TYPE banco_de_registradores IS ARRAY(0 TO 2**E-1) OF STD_LOGIC_VECTOR(N-1 DOWNTO 0);
 SIGNAL MEM_BANCO_REG : banco_de_registradores;
 BEGIN
     PROCESS(CLOCK,REG1_IN,REG2_IN,MEM_BANCO_REG)
